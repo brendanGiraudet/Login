4a827580d1c70f737524103ed3d1a996d70091e3
baka <bakayarusama@gmail.com>
2021-12-24 13:25:15 +0100

    to generate database

---
+++ Login/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Login.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+        public static void InitialiazeDatabase(this WebApplication webApplication)
+        {
+            using IServiceScope? serviceScope = webApplication.Services.GetService<IServiceScopeFactory>()?.CreateScope();
+
+            serviceScope?.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+            serviceScope?.ServiceProvider.GetRequiredService<ConfigurationDbContext>().Database.Migrate();
+        }
+    }
+}
--- Login/Program.cs
+++ Login/Program.cs
@@ -1,9 +1,41 @@
+using System.Reflection;
+using Login.Extensions;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+var connectionString = builder.Configuration.GetConnectionString("Login");
+var migrationsAssembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
+
+// configure identity server with in-memory stores, keys, clients and scopes
+builder.Services.AddIdentityServer()
+    .AddDeveloperSigningCredential()
+    // this adds the config data from DB (clients, resources)
+    .AddConfigurationStore(options =>
+    {
+        options.ConfigureDbContext = builder =>
+            builder.UseSqlite(connectionString,
+                sql => sql.MigrationsAssembly(migrationsAssembly));
+    })
+    // this adds the operational data from DB (codes, tokens, consents)
+    .AddOperationalStore(options =>
+    {
+        options.ConfigureDbContext = builder =>
+            builder.UseSqlite(connectionString,
+                sql => sql.MigrationsAssembly(migrationsAssembly));
+
+        // this enables automatic token cleanup. this is optional.
+        options.EnableTokenCleanup = true;
+        options.TokenCleanupInterval = 30;
+    });
+
 var app = builder.Build();
+
+// Initialize database : apply migrations
+app.InitialiazeDatabase();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
